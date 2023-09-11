3dd615bb6a3ea2c77474a2484f7ffb72b061c29b
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-12 02:20:21 +0530

    cdcd

---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,27 @@
+import EditTopicForm from '@/components/EditTopicForm';
+
+const getTopicById = async (id) => {
+  const apiUrl = process.env.API_URL;
+
+  try {
+    const res = await fetch(`${apiUrl}/api/topics/${id}`, {
+      cache: 'no-store',
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch topic');
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function EditTopic({ params }) {
+  const { id } = params;
+  const { topic } = await getTopicById(id);
+  const { title, description } = topic;
+
+  return <EditTopicForm id={id} title={title} description={description} />;
+}
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import TopicsList from '@/components/TopicsList';
+
+export default function Home() {
+  return (
+    <div>
+      {/* <TopicsList /> */}
+      Hello
+    </div>
+  );
+}
